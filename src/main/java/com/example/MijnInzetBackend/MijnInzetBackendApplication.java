10ac6074875f489b88b7e3c5a8cf3d9f2db5e3a3
Kah Kit <Kazheng@hotmail.nl>
2019-02-28 11:58:15 +0100

    init
    spring boot initializer

---
+++ src/main/java/com/example/MijnInzetBackend/MijnInzetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.MijnInzetBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MijnInzetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MijnInzetBackendApplication.class, args);
+	}
+
+}
